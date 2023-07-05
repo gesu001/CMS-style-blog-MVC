d15bc12185dbcc1f50343f130860328809218939
Ge Su <gegege.su@gmail.com>
2023-07-05 13:25:26 +1000

    added connection.js file and adjusted server.js

---
+++ config/connection.js
@@ -0,0 +1,26 @@
+const Sequelize = require('sequelize');
+
+// Enable access to .env variables
+require('dotenv').config()
+
+// Create a connection object
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    // Use environment variables to connect to database
+    sequelize = new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            // Database location
+            host: 'localhost',
+            dialect: 'mysql',
+            port: 3306
+        }
+    );
+}
+
+module.exports = sequelize;
--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 // import express.js package
 const express = require('express');
+
 // import express-session package for cookie
 const session = require('express-session');
+
 // import express-handlebars package for Handlebars view engine 
 const exphbs = require('express-handlebars');
+
 //Import built-in Node.js package 'path' to resolve path of files that are located on the server; 
 const path = require('path');
-//Initialize an instance of Express.js
+
+// Import the connection object
 const sequelize = require('./config/connection');
+
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+//Initialize an instance of Express.js
 const app = express();
+
 // define what port the web server will listen on(or what por the express.js will run on); whatever is in the environment variable PORT, or 3000 if there's nothing there.
 const PORT = process.env.PORT || 3001;
+
 // Set up sessions with cookies
 const sess = {
     secret: 'secret',
@@ -37,12 +45,16 @@ app.set('view engine', 'handlebars');
 
 // express.json() is a built in middleware function in Express. It parses incoming JSON requests and puts the parsed data in req.body. Without `express.json()`, `req.body` is undefined.  express.json() is a body parser for post request except html post form.
 app.use(express.json());
-//// Sets up the Express app to handle data parsing. it's a body parser for html post form.
+
+// Sets up the Express app to handle data parsing. it's a body parser for html post form.
 app.use(express.urlencoded({ extended: true}));
+
 // Static middleware pointing to the public folder; Using path.join(__dirname, 'public') will create an absolute path, using the directory where app.js is located as the base.
 app.use(express.static(path.join(_dirname, 'public')));
 
 app.use(routes);
+
+// Connect to the database before starting the Express.js server
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, ()=> console.log('Now, listening'));
 });
