824f86a60d84bb205d183cc0deae6ed4c980f7b5
Ge Su <gegege.su@gmail.com>
2023-07-10 16:59:21 +1000

    updated dashboard and login routes and handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -54,6 +54,29 @@ router.get('/post/:id', async (req, res) => {
     }
 });
 
+// Use withAuth middleware to prevent access to route
+router.get('/dashboard', async (req, res) => {
+    try {
+      console.log(req.session)
+      console.log(req.session.logged_in)
+      // Find the logged in user based on the session ID
+      const userData = await User.findByPk(req.session.user_id, {
+        attributes: { exclude: ['password'] },
+        include: [{ model: Post }],
+      });
+  
+      const user = userData.get({ plain: true });
+      console.log(user)
+  
+      res.render('dashboard', {
+        ...user,
+        logged_in: true
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
 router.get('/login', (req, res) => {
     if (req.session.logged_in) {
         res.redirect('/')
@@ -61,4 +84,5 @@ router.get('/login', (req, res) => {
     }
     res.render('login')
 });
+
 module.exports = router;
