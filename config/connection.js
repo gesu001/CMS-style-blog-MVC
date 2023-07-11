0eb436af349e2044e098f97d40c7afcc7db5c69d
Ge Su <gegege.su@gmail.com>
2023-07-11 16:18:32 +1000

    added env file

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,14 @@
 const Sequelize = require('sequelize');
-
-// Enable access to .env variables
-require('dotenv').config()
-
-// Create a connection object
-let sequelize;
+require('dotenv').config();
 
 if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-    // Use environment variables to connect to database
     sequelize = new Sequelize(
         process.env.DB_NAME,
         process.env.DB_USER,
         process.env.DB_PASSWORD,
         {
-            // Database location
             host: 'localhost',
             dialect: 'mysql',
             port: 3306
